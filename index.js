4ec3e918b0a14de2567bffafb1ec29d140c01c88
Maheedhar <maheedharambadipudi756@gmail.com>
2025-08-12 12:05:48 +0530

    exporting cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(cors())
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 })
